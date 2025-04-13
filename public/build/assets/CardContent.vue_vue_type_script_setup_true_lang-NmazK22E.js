a68a16d68a556b10e8030f257004f6633caf4278
Raghd Alshoufani <121362316+Raghdkun@users.noreply.github.com>
2025-04-13 16:14:10 -0400

    ui

---
+++ public/build/assets/CardContent.vue_vue_type_script_setup_true_lang-NmazK22E.js
@@ -0,0 +1,6 @@
+import{c as n}from"./createLucideIcon-CMlEOo_P.js";import{c as a}from"./Button.vue_vue_type_script_setup_true_lang-PJXKnqlO.js";import{d as r,a as t,o as c,n as p,u as l,s as m}from"./app-B91Zl0Oo.js";/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const C=n("ChevronDownIcon",[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]]),_=r({__name:"CardContent",props:{class:{}},setup(s){const o=s;return(e,d)=>(c(),t("div",{class:p(l(a)("p-6 pt-0",o.class))},[m(e.$slots,"default")],2))}});export{C,_};
