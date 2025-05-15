eca9ec09074d3bb3d2a915e87c12bdcfde0d74bf
charlie-miller-work <charliemiller1118@gmail.com>
2025-05-15 11:40:47 -0400

    fixes

--- resources/js/pages/RepairOrders/Index.vue
+++ resources/js/pages/RepairOrders/Index.vue
@@ -2,7 +2,7 @@
     <AppLayout :breadcrumbs="breadcrumbs" :tenantSlug="tenantSlug">
         <Head title="Repair Orders" />
         <!-- responsive here -->
-        <div class="w-full  m-0 p-2 md:p-4 lg:p-6 space-y-2 md:space-y-4 lg:space-y-6">
+        <div class="w-full lg:max-w-7xl lg:mx-auto m-0 p-2 md:p-4 lg:p-6 space-y-2 md:space-y-4 lg:space-y-6">
             
             <!-- Success Message -->
             <Alert v-if="successMessage" variant="success">
--- resources/js/pages/UserRolesManagement.vue
+++ resources/js/pages/UserRolesManagement.vue
@@ -187,7 +187,7 @@ const tenantsArray = computed(() => props.tenants.data);
   <!-- Main layout with breadcrumbs passed to the AppLayout component -->
   <AppLayout  :breadcrumbs="breadcrumbs" :tenantSlug="tenantSlug">
     
-    <div class="container w-full  m-0 p-2 md:p-4 lg:p-6 space-y-2 md:space-y-4 lg:space-y-6">
+    <div class="container w-full  lg:max-w-7xl lg:mx-auto m-0 p-2 md:p-4 lg:p-6 space-y-2 md:space-y-4 lg:space-y-6">
       <!-- Page header with title and description -->
       <div class="space-y-2">
         <h1 class="text-lg md:text-xl lg:text-2xl font-bold text-gray-900 dark:text-gray-100">
