3c97c0e893d90652c8edc3486bc0c9281b6daebe
Aws12505 <awsaljaramani12505@gmail.com>
2025-05-21 08:11:11 -0400

    mail

--- config/mail.php
+++ config/mail.php
@@ -41,7 +41,7 @@ return [
             'transport'      => 'smtp',
             'host'           => env('MAIL_HOST', '127.0.0.1'),
             'port'           => env('MAIL_PORT', 587),
-            'encryption'     => env('MAIL_ENCRYPTION', 'tls'),
+            'encryption'     => env('MAIL_ENCRYPTION', 'ssl'),
             'username'       => env('MAIL_USERNAME'),
             'password'       => env('MAIL_PASSWORD'),
             'timeout'        => null,
