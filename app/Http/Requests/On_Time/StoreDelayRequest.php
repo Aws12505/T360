6a08b7697ce7e9d65e9ff913c5a3f96f37cf58b6
Aws12505 <awsaljaramani12505@gmail.com>
2026-03-28 12:28:48 -0400

    ..

--- app/Http/Requests/On_Time/StoreDelayRequest.php
+++ app/Http/Requests/On_Time/StoreDelayRequest.php
@@ -20,22 +20,26 @@ class StoreDelayRequest extends FormRequest
             : $this->input('tenant_id');
 
         return [
-            'date'                   => 'required|date',
-            'delay_type'             => 'required|in:origin,destination',
-            'driver_name'            => 'nullable|string|max:75',
-            'delay_duration_hours'   => 'required|integer|min:0',
+            'date' => 'required|date',
+            'delay_type' => 'required|in:origin,destination',
+            'driver_name' => 'nullable|string|max:75',
+            'delay_duration_hours' => 'required|integer|min:0',
             'delay_duration_minutes' => 'required|integer|min:0|max:59',
-            'delay_reason'           => 'nullable|string',
-            'load_id'                => [
+            'delay_reason' => 'nullable|string',
+            'load_id' => [
                 'nullable',
                 'string',
                 Rule::unique('delays', 'load_id')
-                    ->where('tenant_id', $tenantId),
+                    ->where(
+                        fn($query) => $query
+                            ->where('tenant_id', $tenantId)
+                            ->where('delay_type', $this->input('delay_type'))
+                    ),
             ],
-            'disputed'               => 'nullable|in:none,pending,won,lost',
-            'driver_controllable'    => 'nullable|boolean',
-            'carrier_controllable'   => 'nullable|boolean',
-            'tenant_id'              => 'required|exists:tenants,id',
+            'disputed' => 'nullable|in:none,pending,won,lost',
+            'driver_controllable' => 'nullable|boolean',
+            'carrier_controllable' => 'nullable|boolean',
+            'tenant_id' => 'required|exists:tenants,id',
         ];
     }
 
